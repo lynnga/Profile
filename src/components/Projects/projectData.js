13ac208d207377cd0c2a51ba66c4a8b127a78d25
Lin Gamliel <lingamliel@gmail.com>
2023-01-30 22:00:51 -0600

    completed project

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,25 +1,33 @@
 let ProjectData = [
     {
         "title":"Social Netwrok Api",
-        "url": "https://github.com/lynnga/weatherApp",
-        "image_url": "https://w0.peakpx.com/wallpaper/137/873/HD-wallpaper-palette-abstract-color-colour-lines-n1-oppo.jpg",
-        "description": "description 1",
-        "skills": ["skill 1", "skill 2", "skill 3"]
+        "url": "https://github.com/lynnga/Social-Network-API",
+        "image_url": "https://www.aplustopper.com/wp-content/uploads/2019/11/Social-Network.jpg",
+        "description": "I built an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. ",
+        "skills": ["Express JS", "MongoDB", "mongoose ODM"]
     },
     {
-        "title":"title 2",
-        "url": "https://github.com/lynnga/weatherApp",
-        "image_url": "https://w0.peakpx.com/wallpaper/137/873/HD-wallpaper-palette-abstract-color-colour-lines-n1-oppo.jpg",
-        "description": "description 2",
-        "skills": ["skill 1", "skill 2", "skill 3"]
+        "title":"Password Generator",
+        "url": " https://github.com/lynnga/Password-gen",
+        "image_url": "https://i.pcmag.com/imagery/articles/04I673XNgIeGMVyO8yL1Bgw-6..v1569489304.jpg",
+        "description": "create an application that generates a random password based on criteria",
+        "skills": ["HTML", "CSS", "JAVESCRIPT"]
     },
     {
-        "title":"title 3",
+        "title":"Weather Dashboard",
         "url": "https://github.com/lynnga/weatherApp",
-        "image_url": "https://w0.peakpx.com/wallpaper/137/873/HD-wallpaper-palette-abstract-color-colour-lines-n1-oppo.jpg",
-        "description": "description 3",
-        "skills": ["skill 1", "skill 2", "skill 3"]
+        "image_url": "https://www.almanac.com/sites/default/files/image_nodes/weather_update_us_map_1.jpg",
+        "description": "Built a weather dashboard that runs in the browser and feature dynamically updated HTML and CSS.",
+        "skills": ["Local Storage", "API", "HTML"]
     },
+    {
+        "title":"Work Day Scheduler",
+        "url": "https://github.com/lynnga/calander-hourly-application",
+        "image_url": "https://www.typecalendar.com/wp-content/uploads/2022/11/November-2023-Calendar.jpg",
+        "description": "calendar application that allows a user to save events for each hour of the day.",
+        "skills": ["Moment", "CSS", "HTML"]
+    },
+    
 ]
 
 export default ProjectData
